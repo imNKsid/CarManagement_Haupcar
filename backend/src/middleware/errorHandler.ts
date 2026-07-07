37c0a535b9e4faac4c24b7763477cac4f259525c
Nitesh <imnksid@gmail.com>
2026-07-07 14:51:45 +0700

    Added errorHandler file to handle different kinds of error

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+
+export class ApiError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export function errorHandler(
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  console.error(err);
+  return res.status(500).json({ message: "Internal server error" });
+}
+
+export function notFoundHandler(_req: Request, res: Response) {
+  res.status(404).json({ message: "Route not found" });
+}
